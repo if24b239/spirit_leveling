00f9ac132fc5027ac2f8859ec406171db7da3d28
if24b239 <if24b239@technikum-wien.at>
2025-05-09 15:59:22 +0200

    initial commit

---
+++ src/main/java/com/rain/spiritleveling/SpiritLevelingDataGenerator.java
@@ -0,0 +1,11 @@
+package com.rain.spiritleveling;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SpiritLevelingDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
