cd6402ea5c1ba51f7fc2d81d45a22b8f73b3a103
if24b239 <if24b239@technikum-wien.at>
2025-06-09 14:20:22 +0200

    datagen and spirit_pill texture

--- src/main/java/com/rain/spiritleveling/SpiritLevelingDataGenerator.java
+++ src/main/java/com/rain/spiritleveling/SpiritLevelingDataGenerator.java
@@ -1,5 +1,7 @@
 package com.rain.spiritleveling;
 
+import com.rain.spiritleveling.datagen.ModModelProvider;
+import com.rain.spiritleveling.datagen.ModRecipeProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
@@ -8,6 +10,9 @@ public class SpiritLevelingDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
-		//FabricDataGenerator.Pack language_pack = fabricDataGenerator.createPack();
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
 	}
 }
